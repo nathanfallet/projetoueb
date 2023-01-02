72f44b0994bb97d6b04b4eea5fbb350a0562e2a9
NathanFallet <contact@nathanfallet.me>
2023-01-02 22:58:33 +0100

    Setting up views

--- tchat/urls.py
+++ tchat/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('channels/new/', views.channels_new, name='channels_new'),
+    path('channels/<int:channel_id>/', views.channels_view, name='channels_view'),
+    path('channels/<int:channel_id>/settings/', views.channels_settings, name='channels_settings'),
+    path('channels/<int:channel_id>/messages/', views.channels_messages, name='channels_messages'),
 ]
--- tchat/views.py
+++ tchat/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import JsonResponse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def register(request):
+    template = loader.get_template('register.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def channels_new(request):
+    template = loader.get_template('channels_new.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def channels_view(request, channel_id):
+    template = loader.get_template('channels_view.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def channels_settings(request, channel_id):
+    template = loader.get_template('channels_settings.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def channels_messages(request, channel_id):
+    return JsonResponse({'channel_id':channel_id})
