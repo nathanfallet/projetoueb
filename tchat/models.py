d91b6b264a18d4f459d24ba406366f5a395812b9
Marie Koza <mariekoza@Maries-MacBook-Air.local>
2023-01-04 19:49:59 +0100

    Objet Implementation

--- tchat/models.py
+++ tchat/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+import datetime 
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    biography = models.TextField()
+    avatar = models.TextField()
+    join_date = models.DateTimeField('inscrit depuis le')
+    last_active = models.DateTimeField()
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=50)
+    logo = models.TextField()
+    created = models.DateTimeField("channel crée le")
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    content = models.TextField()
+    published = models.DateTimeField("message publié le")
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)
+    last_read = models.DateTimeField()
