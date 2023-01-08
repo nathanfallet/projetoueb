77c8e5a47f89a691a05ae5370b07faf124a7b36b
NathanFallet <contact@nathanfallet.me>
2023-01-08 22:12:32 +0100

    Fixing HTML/CSS

--- tchat/static/js/channel.js
+++ tchat/static/js/channel.js
@@ -1,4 +1,14 @@
 function loadConversation(id) {
     // TODO: Appeler /channels/{id}/messages/ pour charger les messages
+    // et les charger dans #channel-messages
     console.log("loadConversation: " + id);
+}
+
+function sendMessage() {
+    // Get message, and clear text
+    let text = $("#message-text").val();
+    $("#message-text").val("");
+
+    // Send it to server (TODO)
+    console.log("sendMessage: " + text);
 }
